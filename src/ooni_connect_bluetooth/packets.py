d1228a6a2d3cab51165b978522d29d818e5de82d
erwin-willems <github-ew@reflexlight.nl>
2026-01-18 15:23:10 +0100

    Fix temperature unit

--- src/ooni_connect_bluetooth/packets.py
+++ src/ooni_connect_bluetooth/packets.py
@@ -91,7 +91,7 @@ class PacketNotify(Packet):
         probe_p1_connected = (flag & 0x04) >> 2  # Probe P1 flag
         probe_p2_connected = (flag & 0x08) >> 3  # Probe P2 flag
         eco_mode = (flag & 0x80) >> 7  # Eco mode flag
-        temperature_unit = TemperatureUnit.CELCIUS if (flag & 0x01) else TemperatureUnit.FARENHEIT  # Temperature unit
+        temperature_unit = TemperatureUnit.CELCIUS if (flag & 0x10) >> 4 else TemperatureUnit.FARENHEIT
 
         return cls(
             battery=battery,
